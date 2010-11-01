417977108ea1f4b30589c1e87d58a3f0c21972ff
Andre Eriksson <cide@ctmod.net>
2010-11-01 01:54:26 +0100

    Fixed whitespace to only use tabs.

--- server/character/handler.py
+++ server/character/handler.py
@@ -2,13 +2,13 @@ from ..mudrpc.character import CharacterService
 from ..mudrpc.character.ttypes import *
 
 class CharacterHandler(object):
-    def __init__(self):
-        self.characters = []
-    def ping(self):
-        print "ping()"
-    def createCharacter(self, name):
-        char = Character(id=(1+len(self.characters)), name=name)
-        self.characters.append(char)
-        return char
-    def getCharacters(self):
-        return self.characters
+	def __init__(self):
+		self.characters = []
+	def ping(self):
+		print "ping()"
+	def createCharacter(self, name):
+		char = Character(id=(1+len(self.characters)), name=name)
+		self.characters.append(char)
+		return char
+	def getCharacters(self):
+		return self.characters
--- test/client.py
+++ test/client.py
@@ -10,33 +10,31 @@ from thrift.transport import TTransport
 from thrift.protocol import TBinaryProtocol
 
 try:
-
-  # Make socket
-  transport = TSocket.TSocket('localhost', 9090)
-
-  # Buffering is critical. Raw sockets are very slow
-  transport = TTransport.TBufferedTransport(transport)
-
-  # Wrap in a protocol
-  protocol = TBinaryProtocol.TBinaryProtocol(transport)
-
-  # Create a client to use the protocol encoder
-  client = CharacterService.Client(protocol)
-
-  # Connect!
-  transport.open()
-
-  client.ping()
-  print 'ping()'
-  
-  # Create a character
-  client.createCharacter("Cide")
-  print client.getCharacters()
-  client.createCharacter("Adys")
-  print client.getCharacters()
-
-  # Close!
-  transport.close()
-
+	# Make socket
+	transport = TSocket.TSocket('localhost', 9090)
+	
+	# Buffering is critical. Raw sockets are very slow
+	transport = TTransport.TBufferedTransport(transport)
+	
+	# Wrap in a protocol
+	protocol = TBinaryProtocol.TBinaryProtocol(transport)
+	
+	# Create a client to use the protocol encoder
+	client = CharacterService.Client(protocol)
+	
+	# Connect!
+	transport.open()
+	
+	client.ping()
+	print 'ping()'
+	
+	# Create a character
+	client.createCharacter("Cide")
+	print client.getCharacters()
+	client.createCharacter("Adys")
+	print client.getCharacters()
+	
+	# Close!
+	transport.close()
 except Thrift.TException, tx:
-  print '%s' % (tx.message)
+	print '%s' % (tx.message)
